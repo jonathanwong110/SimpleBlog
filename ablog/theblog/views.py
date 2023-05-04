fc17a2392b5f72a60c353339f002dffe8c7b0bfc
jonathanwong110 <jonathan.wong110@gmail.com>
2023-05-04 11:51:32 -0400

    slugified category page urls

--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -20,8 +20,8 @@ class HomeView(ListView):
 # an example of what it looks like as a function
 # cats is short for categories
 def CategoryView(request, cats):
-    category_posts = Post.objects.filter(category=cats)
-    return render(request, 'categories.html', {'cats': cats.title(), 'category_posts': category_posts})
+    category_posts = Post.objects.filter(category=cats.replace('-', ' '))
+    return render(request, 'categories.html', {'cats': cats.title().replace('-', ' '), 'category_posts': category_posts})
 
 
 class ArticleDetailView(DetailView):
